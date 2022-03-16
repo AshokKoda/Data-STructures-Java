9a952ea77b97f39802e281f3534ec74b903bd13e
Ashok Koda <kodaashok60@gmail.com>
2022-03-16 19:12:13 +0530

    Added:Data Structures using java

---
+++ src/main/java/com/javadatastructures/DataStructuresInJava.java
@@ -0,0 +1,10 @@
+package com.javadatastructures;
+
+public class DataStructuresInJava {
+
+	public static void main(String[] args) {
+		System.out.println("**************Data STructures*************");
+
+	}
+
+}
