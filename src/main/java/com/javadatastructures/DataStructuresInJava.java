0524535c6fc38014e63d708be5eeeb1fc0a757ae
Ashok Koda <kodaashok60@gmail.com>
2022-03-16 19:35:22 +0530

    Added:UC1/Create a simple linked list

--- src/main/java/com/javadatastructures/DataStructuresInJava.java
+++ src/main/java/com/javadatastructures/DataStructuresInJava.java
@@ -1,9 +1,18 @@
 package com.javadatastructures;
 
+import java.util.LinkedList;
+
 public class DataStructuresInJava {
 
 	public static void main(String[] args) {
 		System.out.println("**************Data STructures*************");
+		
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		list.add(56);
+		list.add(30);
+		list.add(76);
+		
+		System.out.println("LinkedList is : " + list);
 
 	}
 
